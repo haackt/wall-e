ad50fb640e2cc226996ce0a3f5bb64d1d0ed67fb
Torben Haack <haack.t@icloud.com>
2023-01-31 06:44:34 +0100

    Adding configuration to Vite-PWA

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,34 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'Wall-E',
+        name: 'Wall-E WebUI',
+        description: 'Web-UI for the Wall-E bot',
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        id: 'mb.walle.webui',
+        start_url: '/',
+        background_color: '#1f2022',
+        display: 'standalone',
+        scope: '/',
+        theme_color: '#121516',
+        orientation: 'portrait',
+      },
+    }),
+  ],
 });
