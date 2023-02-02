2bfab99fb002a4687544f7bdf8f427d490ce6ede
Torben Haack <haack.t@icloud.com>
2023-02-02 09:48:30 +0100

    Switch from playsound to pygame

--- server/speaker.py
+++ server/speaker.py
@@ -1,7 +1,10 @@
-from playsound import playsound
+import pygame
+
+pygame.mixer.init()
 
 # This function plays a sound file from the sounds folder
 
 
 def play(filename):
-    playsound(f'sounds/{filename}.wav')
+    pygame.mixer.music.load(f'sounds/{filename}.wav')
+    pygame.mixer.music.play()
