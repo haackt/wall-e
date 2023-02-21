cb366270bc2b252265600249b8734452f0a65a23
Torben Haack <haack.t@icloud.com>
2023-02-21 06:47:13 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __COMPILE_TIME__: Date.now()
+  },
   plugins: [
     svelte(),
     VitePWA({
