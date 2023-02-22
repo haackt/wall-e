07366514be774302788d4ab71428c81af87ef8c9
Torben Haack <haack.t@icloud.com>
2023-02-22 05:59:57 +0000

    Bulk commit

--- client/src/lib/components/Dashboard.svelte
+++ client/src/lib/components/Dashboard.svelte
@@ -31,8 +31,14 @@
 <div class="h-full flex flex-col justify-around gap-12 lg:flex-row lg:gap-8">
   <div class="flex flex-col justify-center gap-2 lg:gap-4">
     <h1 class="font-serif text-white text-3xl">Video</h1>
-    <img src={`http://${$ipAddress}:1507/`} alt="Wall-E Live Video Feed" class="aspect-4/3 m-2 bg-black rounded-md" />
-    <ul class="flex flex-nowrap items-center overflow-x-auto text-sm md:text-base gap-4 mt-2 text-mb-text">
+    <img
+      src={`http://${$ipAddress}:1507/`}
+      alt="Wall-E Live Video Feed"
+      class="aspect-4/3 m-2 bg-black rounded-md flex items-center justify-center"
+    />
+    <ul
+      class="flex flex-nowrap items-center overflow-x-auto text-sm md:text-base gap-4 mt-2 text-mb-text snap-x snap-proximity-0 snap-center"
+    >
       <li>
         <DashboardButton onClick={() => servoDialog.show()}>
           <Cog tooths={8} width="w-5" height="h-5" />
